e29e728d7cb4047e1108f548895c09fbda06901c
Dheeraj Thankachan <dheerajthankachan@gmail.com>
2019-11-11 21:09:17 +0000

    Add Multi threading

---
+++ compute/__init__.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import mrcnn.config
+import mrcnn.utils
+from pathlib import Path
+
+# Configuration that will be used by the Mask-RCNN library
+class MaskRCNNConfig(mrcnn.config.Config):
+    NAME = "coco_pretrained_model_config"
+    IMAGES_PER_GPU = 1
+    GPU_COUNT = 1
+    NUM_CLASSES = 1 + 80  # COCO dataset has 80 classes + one background class
+    DETECTION_MIN_CONFIDENCE = 0.6
+
+# Filter a list of Mask R-CNN detection results to get only the detected cars / trucks
+def get_car_boxes(boxes, class_ids):
+    car_boxes = []
+
+    for i, box in enumerate(boxes):
+        # If the detected object isn't a car / truck, skip it
+        if class_ids[i] in [3, 8, 6]:
+            car_boxes.append(box)
+
+    return np.array(car_boxes)
+
+
+def space_Violation(overlaps):
+    space_taken = 0
+    for item in overlaps:
+        # Match Confidante
+        for n in item:
+            if (n < 0.5):
+                pass
+            else:
+                space_taken = space_taken + 1
+    return space_taken
+
+# Root directory of the project
+
+ROOT_DIR = Path(".")
+
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+from threading import Thread
+
+from compute import worker1, worker2
+
 if __name__ == '__main__':
-    import compute.process
+    # Register Workers & Spawn
+    Thread(target=worker1.worker1).start()
+    Thread(target=worker2.worker2).start()
+
+
+
+
 
 
