070175fce8b1baac9c36f98a5d4973feedd7e396
Tom Dawson <tom.dawson@dvla.gov.uk>
2019-11-12 13:20:06 +0000

    fixed JSON formatting

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from threading import Thread
 import requests
+import json
 import time
 from compute import worker1, worker2, worker3
 from parking_state import Settings
@@ -8,15 +9,14 @@ Global = Settings()
 def proccessFeed():
 
     url = 'https://i2yv4ll3q7.execute-api.eu-west-1.amazonaws.com/hack/space/current'
-
-    print(Global.device.state)
-    response = requests.post(url, data=Global.device.state, allow_redirects=True)
+    payload = json.dumps(Global.device.state)
+    print(payload)
+    response = requests.post(url, data=payload, allow_redirects=True)
 
     if response.status_code == 200:
         print("Successfully  posted")
     else:
         print("Error: Bad request")
-
 
 if __name__ == '__main__':
 
