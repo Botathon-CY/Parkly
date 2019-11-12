be99f5f067500f7007f757223ea848caca5e375e
Tom Dawson <tom.dawson@dvla.gov.uk>
2019-11-12 16:48:52 +0000

    WIP adding Kinesis video integration

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from threading import Thread
 import requests
 import json
 import time
-from compute import worker1, worker2, worker3
+from compute import worker1, worker2, worker3, worker4
 from parking_state import Settings
 
 Global = Settings()
@@ -14,24 +14,26 @@ def proccessFeed():
     feed1 = {"name": "NORTH", "spaces": Global.device.state.get("NORTH")}
     feed2 = {"name": "SOUTH", "spaces": Global.device.state.get("SOUTH")}
     feed3 = {"name": "EAST", "spaces": Global.device.state.get("EAST")}
+    liveFeed = {"name": "LIVE", "spaces": Global.device.state.get("LIVE")}
 
-    proccessTemplate = {"name" : "morriston", "parking_areas" : [feed1, feed2, feed3]}
+    proccessTemplate = {"name" : "morriston", "parking_areas" : [feed1, feed2, feed3, liveFeed]}
 
 
-    print(proccessTemplate)
-    response = requests.post(url, data=json.dumps(proccessTemplate), allow_redirects=True)
-
-    if response.status_code == 200:
-        print("Successfully  posted")
-    else:
-        print("Error: Bad request")
+    # print(proccessTemplate)
+    # response = requests.post(url, data=json.dumps(proccessTemplate), allow_redirects=True)
+    #
+    # if response.status_code == 200:
+    #     print("Successfully  posted")
+    # else:
+    #     print("Error: Bad request")
 
 if __name__ == '__main__':
 
     # Register Workers & Spawn
-    Thread(target=worker1.worker1).start()
-    Thread(target=worker2.worker2).start()
-    Thread(target=worker3.worker3).start()
+    # Thread(target=worker1.worker1).start()
+    # Thread(target=worker2.worker2).start()
+    # Thread(target=worker3.worker3).start()
+    Thread(target=worker4.worker4).start()
 
     while True:
         if len(Global.device.state) > 0:
